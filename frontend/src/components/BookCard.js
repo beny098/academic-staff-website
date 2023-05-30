67fb051e7fea30cbce609c2695233fa35e2f9f60
beny098 <83893615+beny098@users.noreply.github.com>
2023-05-30 12:16:01 +1200

    Update BookCard.js

--- frontend/src/components/BookCard.js
+++ frontend/src/components/BookCard.js
@@ -8,7 +8,7 @@ const ProfileCard = (props) => {
     return(
         <div className="card-container">
             <Link to={`/show-book/${book._id}`}>
-            <img src="https://www.seekpng.com/png/full/966-9665493_my-profile-icon-blank-profile-image-circle.png" alt="" />
+            <img src="https://icons.iconarchive.com/icons/paomedia/small-n-flat/256/profile-icon.png" alt="" />
             </Link>
             <div className="desc">
                 <h2>
